6ab2e55d5c750d897bc9dd26c61a4c0bb08954fd
kouyou13241 <ian6472@sina.com>
2021-03-25 21:03:23 +0800

    homework4

---
+++ homework4/时钟事件/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public delegate void ClockHandler(object sender, ClockTime clockTime);
+    public class ClockTime
+    {
+        public int Hour { get; set; }
+        public int Minute { get; set; }
+        public int Second { get; set; }
+        public ClockTime() { }
+        public ClockTime(int hour, int minute, int second)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+        }
+        public void SetClockTime(int hour, int minute, int second)
+        {
+            Hour = hour;
+            Minute = minute;
+            Second = second;
+        }
+    }
+
+    public class ClockOn
+    {
+        public event ClockHandler Tick;
+        public event ClockHandler Alarm;
+
+        public ClockTime AlartClockTime;
+
+        public void ClockTick(ClockTime clockTime)
+        {
+            if (clockTime.Second < 59)
+            {
+                clockTime.Second++;
+            }
+            else if (clockTime.Second >= 59)
+            {
+                clockTime.Second = 0;
+                clockTime.Minute++;
+            }
+            else if (clockTime.Minute >= 59)
+            {
+                clockTime.Minute = 0;
+                clockTime.Hour++;
+            }
+            else if (clockTime.Hour >= 23)
+            {
+                clockTime.Hour = 0;
+            }
+            Console.WriteLine($"现在时间是{clockTime.Hour}:{clockTime.Minute}:{clockTime.Second}");
+            Tick(this, clockTime);
+        }
+
+        public void ClockAlarm(ClockTime clockTime, int hour, int minute, int second)
+        {
+            if (clockTime.Second == second && clockTime.Minute == minute && clockTime.Hour == hour)
+            {
+                Console.WriteLine("滴滴滴！！！");
+            }
+            Alarm(this, clockTime);
+        }
+    }
+
+    class Form
+    {
+        public ClockOn Clockon = new ClockOn();
+        public Form()
+        {
+            Clockon.Tick += Clockon_Tick;
+            Clockon.Alarm += Clockon_Alarm;
+        }
+
+        private void Clockon_Alarm(object sender, ClockTime clockTime)
+        {
+         
+            Console.WriteLine("闹钟设置......");
+        }
+
+        private void Clockon_Tick(object sender, ClockTime clockTime)
+        {
+         
+            Console.WriteLine("时钟在走！");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClockTime clockTime = new ClockTime(0, 0, 0);
+            ClockOn clockOn = new ClockOn();
+            Form form1 = new Form();
+
+            for (int i = 0; i <= 602; i++)
+            {
+                form1.Clockon.ClockTick(clockTime);
+                form1.Clockon.ClockAlarm(clockTime, 0, 3, 20);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ homework4/链表/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> temp = head;
+            while (temp != null)
+            {
+                action(temp.Data);
+                temp = temp.Next;
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+
+            int ans = 0;
+            int max = 0;
+            int min = 1000;
+            intlist.ForEach((item) => Console.WriteLine(item)); // 打印链表元素
+            intlist.ForEach((item) => ans = ans + item);// 求和
+            intlist.ForEach((item) =>
+            {
+                if (max < item)
+                    max = item;
+            });  // 求最大值
+            intlist.ForEach((item) =>
+            {
+                if (min > item)
+                    min = item;
+            });  // 求最小值
+            Console.WriteLine("求和为" + ans);
+            Console.WriteLine("最大值为" + max);
+            Console.WriteLine("最小值为" + min);
+        }
+    }
+}
